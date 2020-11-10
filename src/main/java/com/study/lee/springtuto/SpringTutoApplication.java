348eec0ac1fcb7df1697b8dc1930ad64130cfc3e
91moderato <91moderato@gmail.com>
2020-11-10 17:08:32 +0900

    초기 등록

---
+++ src/main/java/com/study/lee/springtuto/SpringTutoApplication.java
@@ -0,0 +1,13 @@
+package com.study.lee.springtuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutoApplication.class, args);
+    }
+
+}
